5e52fa33da40ac3a5b70da83b605a5a86c21a10c
brianartea <bri@expertdesign.cc>
2022-04-02 12:41:47 -0400

    commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Memories
+
+![Memories](https://expertdesign.cc/memories/screenshot-memories-app.jpg)
+
+## Introduction
+
+This is a code repository for the corresponding video tutorial - https://youtube.com/playlist?list=PL6QREj8te1P7VSwhrMf3D3Xt4V6_SRkhu.
+
+Using React, Node.js, Express & MongoDB you'll learn how to build a Full Stack MERN Application - from start to finish. The App is called "Memories" and it is a simple social media app that allows users to post interesting events that happened in their lives.
+
+The server folder is in a zip file. You will need to unzip for it to work.
+
+By the end of this video, you will have a strong understanding of how the MERN Stack works.
+
+Setup:
+
+- run `npm i && npm start` for both client and server side to start the app
+
+TODO: rename post message, refactor reducers, add some cool memories, maybe clickable tags, fix update functionality. I did a little extra styling.
---
+++ client/src/components/PostDetails/styles.js
@@ -0,0 +1,64 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+export default makeStyles((theme) => ({
+  media: {
+    borderRadius: "20px",
+    objectFit: "cover",
+    width: "100%",
+    maxHeight: "600px",
+  },
+  card: {
+    display: "flex",
+    width: "100%",
+    [theme.breakpoints.down("sm")]: {
+      flexWrap: "wrap",
+      flexDirection: "column",
+    },
+  },
+  section: {
+    borderRadius: "20px",
+    margin: "10px",
+    flex: 1,
+  },
+  imageSection: {
+    marginLeft: "20px",
+    [theme.breakpoints.down("sm")]: {
+      marginLeft: 0,
+    },
+  },
+  recommendedPosts: {
+    display: "flex",
+    [theme.breakpoints.down("sm")]: {
+      flexDirection: "column",
+    },
+  },
+  loadingPaper: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    padding: "20px",
+    borderRadius: "15px",
+    height: "39vh",
+  },
+  commentsOuterContainer: {
+    display: "flex",
+    justifyContent: "space-between",
+  },
+  commentsInnerContainer: {
+    height: "200px",
+    overflowY: "auto",
+    marginRight: "30px",
+  },
+  fontCreatedBy: {
+    fontSize: "16px",
+    fontWeight: "bold",
+    color: "rgba(248, 122, 122, 1)",
+  },
+  fontSizeTime: {
+    fontSize: "14px",
+    color: "#838383",
+  },
+  fontMessage: {
+    fontSize: "18px",
+  },
+}));
